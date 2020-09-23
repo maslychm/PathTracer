037e3ecfcb5d8b66cd67344d00b2dcf61992ae17
maslychm <43874961+maslychm@users.noreply.github.com>
2020-09-23 19:01:09 -0400

    first attempt on lambertian

--- RayTracer/vec3.h
+++ RayTracer/vec3.h
@@ -6,9 +6,6 @@
 
 using std::sqrt;
 
-// Using vec4 for now so it's easier to extend in the future
-// Not needed because saving as .ppm with only RGB
-
 class vec3 {
 public:
 	vec3() : e{ 0,0,0 } {}
@@ -44,21 +41,29 @@ public:
 		return sqrt(length_squared());
 	}
 
-	double length_squared() const{
+	double length_squared() const {
 		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
 	}
-	
+
+	inline static vec3 random() {
+		return vec3(random_double(), random_double(), random_double());
+	}
+
+	inline static vec3 random(double min, double max) {
+		return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+	}
+
 public:
 	double e[3];
 };
 
 
-// Type aliases for vec4
+// Type aliases for vec3
 using point3 = vec3;	// 3D point + homogeneous
 using color = vec3;		// RGBA point
 
 
-// vec4 Untility Functions
+// vec3 Untility Functions
 inline std::ostream& operator << (std::ostream &out, const vec3 &v) {
 	return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
@@ -89,18 +94,26 @@ inline vec3 operator / (vec3 v, double t) {
 
 inline double dot(const vec3 &u, const vec3 &v) {
 	return u.e[0] * v.e[0]
-		 + u.e[1] * v.e[1]
-		 + u.e[2] * v.e[2];
+		+ u.e[1] * v.e[1]
+		+ u.e[2] * v.e[2];
 }
 
 inline vec3 cross(const vec3 &u, const vec3 &v) {
 	return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
-		        u.e[2] * v.e[0] - u.e[0] * v.e[2],
-		        u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+		u.e[2] * v.e[0] - u.e[0] * v.e[2],
+		u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
 inline vec3 unit_vector(vec3 v) {
 	return v / v.length();
+}
+
+vec3 random_in_unit_sphere() {
+	while (true) {
+		auto p = vec3::random(-1, 1);
+		if (p.length_squared() >= 1) continue;
+		return p;
+	}
 }
 
 #endif
