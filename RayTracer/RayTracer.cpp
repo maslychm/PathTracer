e991b9440478e0caf3fb37c918bcf1a63fabfba1
maslychm <43874961+maslychm@users.noreply.github.com>
2021-02-21 02:15:35 -0500

    boundiung volume hierarchies

--- RayTracer/RayTracer.cpp
+++ RayTracer/RayTracer.cpp
@@ -1,5 +1,6 @@
 #include "rtcommon.h"
 
+#include "bvh.h"
 #include "camera.h"
 #include "color.h"
 #include "hittable_list.h"
@@ -44,54 +45,6 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-//hittable_list random_scene() {
-//	hittable_list world;
-//
-//	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-//	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
-//
-//	for (int a = -11; a < 11; a++) {
-//		for (int b = -11; b < 11; b++) {
-//			auto choose_mat = random_double();
-//			point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
-//
-//			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-//				shared_ptr<material> sphere_material;
-//
-//				if (choose_mat < 0.8) {
-//					// diffuse
-//					auto albedo = color::random() * color::random();
-//					sphere_material = make_shared<lambertian>(albedo);
-//					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-//				}
-//				else if (choose_mat < 0.95) {
-//					// metal
-//					auto albedo = color::random(0.5, 1);
-//					auto fuzz = random_double(0, 0.5);
-//					sphere_material = make_shared<metal>(albedo, fuzz);
-//					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-//				}
-//				else {
-//					// glass
-//					sphere_material = make_shared<dielectric>(1.5);
-//					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-//				}
-//			}
-//		}
-//	}
-//
-//	auto material1 = make_shared<dielectric>(1.5);
-//	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-//
-//	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-//	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-//
-//	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-//	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-//
-//	return world;
-//}
-
 hittable_list random_scene() {
 	hittable_list world;
 
@@ -101,18 +54,16 @@ hittable_list random_scene() {
 	for (int a = -11; a < 11; a++) {
 		for (int b = -11; b < 11; b++) {
 			auto choose_mat = random_double();
-			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+			point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
 
-			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
 				shared_ptr<material> sphere_material;
 
 				if (choose_mat < 0.8) {
 					// diffuse
 					auto albedo = color::random() * color::random();
 					sphere_material = make_shared<lambertian>(albedo);
-					auto center2 = center + vec3(0, random_double(0, .5), 0);
-					world.add(make_shared<moving_sphere>(
-						center, center2, 0.0, 1.0, 0.2, sphere_material));
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
 				}
 				else if (choose_mat < 0.95) {
 					// metal
@@ -139,8 +90,58 @@ hittable_list random_scene() {
 	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
 	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
-	return world;
+	return hittable_list(make_shared<bvh_node>(world, 0.0, 1.0));
 }
+
+//hittable_list random_scene() {
+//	hittable_list world;
+//
+//	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+//	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+//
+//	for (int a = -11; a < 11; a++) {
+//		for (int b = -11; b < 11; b++) {
+//			auto choose_mat = random_double();
+//			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+//
+//			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+//				shared_ptr<material> sphere_material;
+//
+//				if (choose_mat < 0.8) {
+//					// diffuse
+//					auto albedo = color::random() * color::random();
+//					sphere_material = make_shared<lambertian>(albedo);
+//					auto center2 = center + vec3(0, random_double(0, .5), 0);
+//					world.add(make_shared<moving_sphere>(
+//						center, center2, 0.0, 1.0, 0.2, sphere_material));
+//				}
+//				else if (choose_mat < 0.95) {
+//					// metal
+//					auto albedo = color::random(0.5, 1);
+//					auto fuzz = random_double(0, 0.5);
+//					sphere_material = make_shared<metal>(albedo, fuzz);
+//					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+//				}
+//				else {
+//					// glass
+//					sphere_material = make_shared<dielectric>(1.5);
+//					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+//				}
+//			}
+//		}
+//	}
+//
+//	auto material1 = make_shared<dielectric>(1.5);
+//	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+//
+//	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+//	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+//
+//	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+//	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+//
+//	return hittable_list(make_shared<bvh_node>(world, 0.0, 1.0));
+//}
 
 int main()
 {
@@ -149,10 +150,10 @@ int main()
 	// Image
 
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 400;
+	const int image_width = 1000;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 10;
-	const int max_depth = 15;
+	const int samples_per_pixel = 100;
+	const int max_depth = 50;
 
 	// World
 
