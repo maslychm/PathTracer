d510aa20eff825506cae71ad0d1f88fbc7bb29eb
maslychm <43874961+maslychm@users.noreply.github.com>
2021-02-24 20:14:32 -0500

    Textures. Checker texture

--- RayTracer/RayTracer.cpp
+++ RayTracer/RayTracer.cpp
@@ -45,11 +45,22 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
+hittable_list two_spheres() {
+	hittable_list objects;
+
+	auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+
+	objects.add(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(checker)));
+	objects.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+
+	return objects;
+}
+
 hittable_list random_scene() {
 	hittable_list world;
 
-	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+	auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
 
 	for (int a = -11; a < 11; a++) {
 		for (int b = -11; b < 11; b++) {
@@ -93,82 +104,46 @@ hittable_list random_scene() {
 	return hittable_list(make_shared<bvh_node>(world, 0.0, 1.0));
 }
 
-//hittable_list random_scene() {
-//	hittable_list world;
-//
-//	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-//	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
-//
-//	for (int a = -11; a < 11; a++) {
-//		for (int b = -11; b < 11; b++) {
-//			auto choose_mat = random_double();
-//			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
-//
-//			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
-//				shared_ptr<material> sphere_material;
-//
-//				if (choose_mat < 0.8) {
-//					// diffuse
-//					auto albedo = color::random() * color::random();
-//					sphere_material = make_shared<lambertian>(albedo);
-//					auto center2 = center + vec3(0, random_double(0, .5), 0);
-//					world.add(make_shared<moving_sphere>(
-//						center, center2, 0.0, 1.0, 0.2, sphere_material));
-//				}
-//				else if (choose_mat < 0.95) {
-//					// metal
-//					auto albedo = color::random(0.5, 1);
-//					auto fuzz = random_double(0, 0.5);
-//					sphere_material = make_shared<metal>(albedo, fuzz);
-//					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-//				}
-//				else {
-//					// glass
-//					sphere_material = make_shared<dielectric>(1.5);
-//					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-//				}
-//			}
-//		}
-//	}
-//
-//	auto material1 = make_shared<dielectric>(1.5);
-//	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-//
-//	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-//	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-//
-//	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-//	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-//
-//	return hittable_list(make_shared<bvh_node>(world, 0.0, 1.0));
-//}
-
 int main()
 {
-	srand((unsigned) time(0));
-
-	// Image
-
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 1000;
-	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 100;
-	const int max_depth = 50;
+	const int image_width = 400;
+	const int samples_per_pixel = 50;
+	const int max_depth = 25;
 
-	// World
+	hittable_list world;
+	
+	point3 lookfrom;
+	point3 lookat;
+	auto vfov = 40.0;
+	auto aperture = 0.0;
 
-	auto world = random_scene();
+	switch (0) {
+	case 1:
+		world = random_scene();
+		lookfrom = point3(13, 2, 3);
+		lookat = point3(0, 0, 0);
+		vfov = 20.0;
+		aperture = 0.1;
+		break;
 
+	default:
+	case 2:
+		world = two_spheres();
+		lookfrom = point3(13, 2, 3);
+		lookat = point3(0, 0, 0);
+		vfov = 20.0;
+		break;
+	}
+	
 	// Camera
 
-	point3 lookfrom(13, 2, 3);
-	point3 lookat(0, 0, 0);
 	vec3 vup(0, 1, 0);
 	auto dist_to_focus = 10.0;
-	auto aperture = 0.1;
+	int image_height = static_cast<int>(image_width / aspect_ratio);
 
-	camera cam = camera(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
-
+	camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
+	
 	// Render
 
 	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
--- RayTracer/sphere.h
+++ RayTracer/sphere.h
@@ -19,6 +19,22 @@ public:
 	point3 center;
 	double radius;
 	shared_ptr<material> mat_ptr;
+
+private:
+	static void get_sphere_uv(const point3& p, double& u, double& v) {
+		// p: a given point on the sphere of radius one, centered at the origin.
+		// u: returned value [0,1] of angle around the Y axis from X=-1.
+		// v: returned value [0,1] of angle from Y=-1 to Y=+1.
+		//     <1 0 0> yields <0.50 0.50>       <-1  0  0> yields <0.00 0.50>
+		//     <0 1 0> yields <0.50 1.00>       < 0 -1  0> yields <0.50 0.00>
+		//     <0 0 1> yields <0.25 0.50>       < 0  0 -1> yields <0.75 0.50>
+
+		auto theta = acos(-p.y());
+		auto phi = atan2(-p.z(), p.x()) + pi;
+
+		u = phi / (2 * pi);
+		v = theta / pi;
+	}
 };
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
@@ -26,33 +42,27 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
 	auto a = r.direction().length_squared();
 	auto half_b = dot(oc, r.direction());
 	auto c = oc.length_squared() - radius * radius;
+
 	auto discriminant = half_b * half_b - a * c;
+	if (discriminant < 0) return false;
+	auto sqrtd = sqrt(discriminant);
 
-	if (discriminant > 0) {
-		auto root = sqrt(discriminant);
-
-		auto temp = (-half_b - root) / a;
-		if (temp < t_max && temp > t_min) {
-			rec.t = temp;
-			rec.p = r.at(rec.t);
-			vec3 outward_normal = (rec.p - center) / radius;
-			rec.set_face_normal(r, outward_normal);
-			rec.mat_ptr = mat_ptr;
-			return true;
-		}
-
-		temp = (-half_b + root) / a;
-		if (temp < t_max && temp > t_min) {
-			rec.t = temp;
-			rec.p = r.at(rec.t);
-			vec3 outward_normal = (rec.p - center) / radius;
-			rec.set_face_normal(r, outward_normal);
-			rec.mat_ptr = mat_ptr;
-			return true;
-		}
+	// Find the nearest root that lies in the acceptable range
+	auto root = (-half_b - sqrtd) / a;
+	if (root < t_min || t_max < root) {
+		root = (-half_b + sqrtd) / a;
+		if (root < t_min || t_max < root)
+			return false;
 	}
 
-	return false;
+	rec.t = root;
+	rec.p = r.at(rec.t);
+	vec3 outward_normal = (rec.p - center) / radius;
+	rec.set_face_normal(r, outward_normal);
+	get_sphere_uv(outward_normal, rec.u, rec.v);
+	rec.mat_ptr = mat_ptr;
+
+	return true;
 }
 
 bool sphere::bounding_box(double time0, double time1, aabb& output_box) const {
